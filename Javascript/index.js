4dc558886b1d99b24c7df8a156602808bc276b1c
Ian Curl <iancurl.salvador21@gmail.com>
2023-12-10 22:03:57 +0800

    Lesson 2 completed about variables

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,11 @@
-console.log("Hello");
-alert("Hatdog");
+let firstName = "Ian Salvador";
+
+let age;
+age = 21;
+
+let student = true;
+
+
+document.getElementById("p1").innerHTML = "Kumusta ka na " + firstName;
+document.getElementById("p2").innerHTML = "ikaw ay " + age + " taong gulang";
+document.getElementById("p3").innerHTML = student + " na nakaenroll ka"; 

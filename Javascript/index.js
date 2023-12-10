51aa7da559d1b8cad1c2b6a3373415cba693650c
Ian Curl <iancurl.salvador21@gmail.com>
2023-12-10 22:40:45 +0800

    done with the lesson type conversion

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,19 @@
-let firstname;
+// let age = window.prompt("How old are you?");
+// console.log(typeof age);
 
-document.getElementById("submit").onclick = function(){
+// age = Number(age);
+// age +=1;
+// console.log(typeof age);
+// console.log("Happy Birthday! YOu are ", age, " years old");
 
-    firstname = document.getElementById("firstName").value;
-    console.log(firstname);
-    document.getElementById("label").innerHTML = "Hello " + firstname;
-}
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(x);
+z = Boolean("y");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
